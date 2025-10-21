c4814d10293ec61318358cedc48e95675e7fbc0f
Ramiro Landajo <ramalandajo@gmail.com>
2025-10-21 12:43:40 -0300

    Added all Dev EB Backends

--- dev/backend.tf
+++ dev/backend.tf
@@ -37,7 +37,7 @@ provider "aws" {
 
  */
 
-# --- Data sources
+# --- Data sources --- 
 data "aws_vpc" "default_vpc_1" {
   provider = aws.admin_1
   default = true
@@ -55,10 +55,13 @@ locals {
   subnets_csv_1 = join(",", data.aws_subnets.default_subnets_1.ids)
 }
 
-resource "aws_elastic_beanstalk_environment" "ecommerce-dev-env" {
+
+// MIDDLEWARE EB BACKEND
+
+resource "aws_elastic_beanstalk_environment" "middleware-dev-env" {
   provider = aws.admin_1
-  name  = "ecommerce-dev-env"  
-  application = aws_elastic_beanstalk_application.ecommerce-dev-app.name
+  name  = "middleware-dev-env"  
+  application = aws_elastic_beanstalk_application.middleware-dev-app.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.6.6 running Corretto 17"
   tags = {
     Type = "EB Backend"
@@ -104,9 +107,9 @@ resource "aws_elastic_beanstalk_environment" "ecommerce-dev-env" {
   }
 }
 
-resource "aws_elastic_beanstalk_application" "ecommerce-dev-app" {
+resource "aws_elastic_beanstalk_application" "middleware-dev-app" {
   provider = aws.admin_1
-  name = "ecommerce-dev-app"
+  name = "middleware-dev-app"
   tags = {
     Type = "EB Backend"
     Stage = "Dev"
@@ -116,7 +119,6 @@ resource "aws_elastic_beanstalk_application" "ecommerce-dev-app" {
     service_role = "arn:aws:iam::746207465310:role/aws-elasticbeanstalk-service-role"
   }
 }
-
 
 /* 
 
@@ -141,6 +143,72 @@ data "aws_subnets" "default_subnets_2" {
 locals {
   subnets_csv_2 = join(",", data.aws_subnets.default_subnets_2.ids)
 }
+
+// ECOMMERCE EB BACKEND
+
+resource "aws_elastic_beanstalk_environment" "ecommerce-dev-env" {
+  provider = aws.admin_2
+  name  = "ecommerce-dev-env"  
+  application = aws_elastic_beanstalk_application.ecommerce-dev-app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.6.6 running Corretto 17"
+  tags = {
+    Type = "EB Backend"
+    Stage = "Dev"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = data.aws_vpc.default_vpc_2.id
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = local.subnets_csv_2
+  }
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "InstanceTypes"
+    value     = var.backend_instance_type
+  }
+    setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "aws-elasticbeanstalk-service-role"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+  namespace = "aws:elasticbeanstalk:environment"
+  name      = "EnvironmentType"
+  value     = "SingleInstance"
+  }
+  dynamic "setting" {
+    for_each = var.env_vars_ecommerce
+    content {
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+    }
+  }
+}
+
+resource "aws_elastic_beanstalk_application" "ecommerce-dev-app" {
+  provider = aws.admin_2
+  name = "ecommerce-dev-app"
+  tags = {
+    Type = "EB Backend"
+    Stage = "Dev"
+  }
+
+  appversion_lifecycle {
+    service_role = "arn:aws:iam::426333730924:role/aws-elasticbeanstalk-service-role"
+  }
+}
+
+// CATALOGUE EB BACKEND
 
 resource "aws_elastic_beanstalk_environment" "catalogue-dev-env" {
   provider = aws.admin_2
@@ -194,6 +262,134 @@ resource "aws_elastic_beanstalk_environment" "catalogue-dev-env" {
 resource "aws_elastic_beanstalk_application" "catalogue-dev-app" {
   provider = aws.admin_2
   name = "catalogue-dev-app"
+  tags = {
+    Type = "EB Backend"
+    Stage = "Dev"
+  }
+
+  appversion_lifecycle {
+    service_role = "arn:aws:iam::426333730924:role/aws-elasticbeanstalk-service-role"
+  }
+}
+
+// ANALYTICS EB BACKEND
+
+resource "aws_elastic_beanstalk_environment" "analytics-dev-env" {
+  provider = aws.admin_2
+  name  = "analytics-dev-env"  
+  application = aws_elastic_beanstalk_application.analytics-dev-app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.6.6 running Corretto 17"
+  tags = {
+    Type = "EB Backend"
+    Stage = "Dev"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = data.aws_vpc.default_vpc_2.id
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = local.subnets_csv_2
+  }
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "InstanceTypes"
+    value     = var.backend_instance_type
+  }
+    setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "aws-elasticbeanstalk-service-role"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+  namespace = "aws:elasticbeanstalk:environment"
+  name      = "EnvironmentType"
+  value     = "SingleInstance"
+  }
+  dynamic "setting" {
+    for_each = var.env_vars_ecommerce
+    content {
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+    }
+  }
+}
+
+resource "aws_elastic_beanstalk_application" "analytics-dev-app" {
+  provider = aws.admin_2
+  name = "analytics-dev-app"
+  tags = {
+    Type = "EB Backend"
+    Stage = "Dev"
+  }
+
+  appversion_lifecycle {
+    service_role = "arn:aws:iam::426333730924:role/aws-elasticbeanstalk-service-role"
+  }
+}
+
+// CORE EB BACKEND
+
+resource "aws_elastic_beanstalk_environment" "core-dev-env" {
+  provider = aws.admin_2
+  name  = "core-dev-env"  
+  application = aws_elastic_beanstalk_application.core-dev-app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.6.6 running Corretto 17"
+  tags = {
+    Type = "EB Backend"
+    Stage = "Dev"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = data.aws_vpc.default_vpc_2.id
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = local.subnets_csv_2
+  }
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "InstanceTypes"
+    value     = var.backend_instance_type
+  }
+    setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "aws-elasticbeanstalk-service-role"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+  setting {
+  namespace = "aws:elasticbeanstalk:environment"
+  name      = "EnvironmentType"
+  value     = "SingleInstance"
+  }
+  dynamic "setting" {
+    for_each = var.env_vars_ecommerce
+    content {
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+    }
+  }
+}
+
+resource "aws_elastic_beanstalk_application" "core-dev-app" {
+  provider = aws.admin_2
+  name = "core-dev-app"
   tags = {
     Type = "EB Backend"
     Stage = "Dev"
