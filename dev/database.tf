3d3db4f9dff0a0afa1fb33cc386cfa2455bd7d33
Ramiro Landajo <ramalandajo@gmail.com>
2025-11-08 22:06:53 -0300

    Added missing files in dev env

--- dev/database.tf
+++ dev/database.tf
@@ -11,8 +11,30 @@ resource "aws_db_instance" "mysql" {
   deletion_protection     = false
   auto_minor_version_upgrade = true
   backup_retention_period = 0
+  vpc_security_group_ids = [aws_security_group.rds_mysql.id]
+  
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
 
-  lifecycle {
-    prevent_destroy = true
+# Security Group for RDS MySQL allowing access
+resource "aws_security_group" "rds_mysql" {
+  provider    = aws.admin_3
+  name        = "rds-mysql-public"
+  vpc_id      = data.aws_vpc.default_vpc_3.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
